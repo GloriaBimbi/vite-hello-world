36e25ab769c1b3b6ef8e1612ae0e07480572623a
GloriaBimbi <gloria.bimbi1@libero.it>
2024-02-07 16:22:22 +0100

    fix folder

--- first-project/src/components/AppTitle.vue
+++
@@ -1,9 +0,0 @@
-<script>
-export default {};
-</script>
-
-<template>
-  <h1>La mia prima app con Vite!</h1>
-</template>
-
-<style></style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,14 @@
+<script>
+import AppHero from "./AppHero.vue";
+export default {
+  components: { AppHero },
+};
+</script>
+
+<template>
+  <AppHero>
+    <h1>La mia prima app con Vite!</h1>
+  </AppHero>
+</template>
+
+<style></style>
